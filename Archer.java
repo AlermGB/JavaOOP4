2f883df4586f298fa584908e18036f0b94cc69bc
Alexandr <kaperisation@mail.ru>
2023-04-14 18:39:29 +0300

    init

---
+++ Archer.java
@@ -0,0 +1,16 @@
+public class Archer extends Warrior<Throwing> {
+    public Archer(String name, int hp, Throwing weapon) {
+        super(name, hp, weapon);
+    }
+
+    public int getRange() {
+        return rand.nextInt(weapon.getDistance() + 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Archer{" +
+                super.toString() +
+                '}';
+    }
+}
---
+++ Knight.java
@@ -0,0 +1,12 @@
+public class Knight extends Warrior<Knife> {
+    public Knight(String name, int hp, Knife weapon) {
+        super(name, hp, weapon);
+    }
+
+    @Override
+    public String toString() {
+        return "Knight{" +
+                super.toString() +
+                '}';
+    }
+}
